46806d27ecf772a611a150e9d0b125e5e1a01bd4
jiayang <jiayang@yuntech.edu.tw>
2021-02-15 23:48:20 +0800

    <refactor>: (refactor test method) </refactor>
    重構amount測試案例
    直接使用assert.equal判斷dollar.times是否等於new dollar(int)
    
    money.cs
    amount屬性更改為唯讀：private set

--- csharp/money/src/money/Class1.cs
+++ csharp/money/src/money/Class1.cs
@@ -4,7 +4,7 @@ namespace money
 {
     public class Dollar
     {
-        public int amount{ get; set; }
+        public int amount{ get; private set; }
         public Dollar(int _amount)
         {
             amount = _amount;
