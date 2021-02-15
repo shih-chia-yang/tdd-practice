44f3a3c2fdd1b750ebb53adb94ac27507a1a2777
jiayang <jiayang@yuntech.edu.tw>
2021-02-15 23:43:33 +0800

    <feature>: (實作equals方法) </feature>
    以判斷值來確定物件是否相等

--- csharp/money/src/money/Class1.cs
+++ csharp/money/src/money/Class1.cs
@@ -15,24 +15,20 @@ namespace money
             return new Dollar(amount * multiplier);
         }
 
-        public bool Equals(object obj)
+        public override bool Equals(object obj)
         {
-            // //
-            // // See the full list of guidelines at
-            // //   http://go.microsoft.com/fwlink/?LinkID=85237
-            // // and also the guidance for operator== at
-            // //   http://go.microsoft.com/fwlink/?LinkId=85238
-            // //
-
-            // if (obj == null || GetType() != obj.GetType())
-            // {
-            //     return false;
-            // }
-
-            // // TODO: write your implementation of Equals() here
-            // throw new System.NotImplementedException();
-            // return base.Equals (obj);
-            return true;
+            if (obj == null || GetType() != obj.GetType())
+            {
+                return false;
+            }
+            if(this.amount==((Dollar)obj).amount)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
         
         // public override int GetHashCode()
