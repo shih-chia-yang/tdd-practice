7ff3709064afbdcd75fff55c86370fdbef008cdd
jiayang <jiayang@yuntech.edu.tw>
2021-02-16 00:28:17 +0800

    <style>: (刪除已註記程式) </style>

--- csharp/money/src/money/Dollar.cs
+++ csharp/money/src/money/Dollar.cs
@@ -4,7 +4,6 @@ namespace money
 {
     public class Dollar:Money
     {
-        // public int amount{ get; private set; }
         public Dollar(int amount)
         {
             _amount = amount;
@@ -14,22 +13,5 @@ namespace money
         {
             return new Dollar(amount * multiplier);
         }
-
-        // public override bool Equals(object obj)
-        // {
-        //     if (obj == null || GetType() != obj.GetType())
-        //     {
-        //         return false;
-        //     }
-        //     Money money = (Dollar)obj;
-        //     return this.amount == money.amount;
-        // }
-        
-        // public override int GetHashCode()
-        // {
-        //     // TODO: write your implementation of GetHashCode() here
-        //     // throw new System.NotImplementedException();
-        //     return base.GetHashCode();
-        // }
     }
 }
--- csharp/money/src/money/Franc.cs
+++ csharp/money/src/money/Franc.cs
@@ -4,7 +4,6 @@ namespace money
 {
     public class Franc:Money
     {
-        //  public int amount{ get; private set; }
         public Franc(int amount)
         {
             _amount = amount;
@@ -14,22 +13,5 @@ namespace money
         {
             return new Franc(amount * multiplier);
         }
-
-        // public override bool Equals(object obj)
-        // {
-        //     if (obj == null || GetType() != obj.GetType())
-        //     {
-        //         return false;
-        //     }
-        //     Money money = (Franc)obj;
-        //     return this.amount == money.amount;
-        // }
-        
-        // public override int GetHashCode()
-        // {
-        //     // TODO: write your implementation of GetHashCode() here
-        //     // throw new System.NotImplementedException();
-        //     return base.GetHashCode();
-        // }
     }
 }
