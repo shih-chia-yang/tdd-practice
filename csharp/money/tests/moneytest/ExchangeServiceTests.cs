7c8f2be8b0fbdd05d9212c58c09d0e75ad2555da
jiayang <jiayang@yuntech.edu.tw>
2021-02-18 17:30:51 +0800

    <test>: (add TestCase franc plus) </test>
    增加franc案例
    update refactor-money-valueobject.md
    測試失敗
    原因：Plus方法只能回傳美元

--- csharp/money/tests/moneytest/ExchangeServiceTests.cs
+++ csharp/money/tests/moneytest/ExchangeServiceTests.cs
@@ -14,10 +14,13 @@ namespace moneytest
             //Given
             IExchangeService exchange = new ExchangeService();
             var fiveBucks = Bank.Dollar(5);
+            var fiveFranc = Bank.Franc(5);
             //When
-            Money result=exchange.Plus(new Money[]{fiveBucks, fiveBucks});
+            Money dollarResult=exchange.Plus(new Money[]{fiveBucks, fiveBucks});
+            Money francResult=exchange.Plus(new Money[]{fiveBucks, fiveBucks});
             //Then
-            Assert.Equal(Bank.Dollar(10), result);
+            Assert.Equal(Bank.Dollar(10), dollarResult);
+            Assert.Equal(Bank.Franc(10), francResult);
         }
     }
 }
