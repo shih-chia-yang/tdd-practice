30d6c50c7d65d8acc2887609cefbdb88184b73f9
jiayang <jiayang@yuntech.edu.tw>
2021-02-22 16:10:47 +0800

    <feature>:(add python-functions)</feature>
    create say_hello()

---
+++ python/python-functions/exercise1.py
@@ -0,0 +1,11 @@
+
+#define function 'def' keyword
+# a pair parentheses symbols (), input parameters are defined within these parantheses
+# a colon symbol : to end the function's signature
+
+def say_hello():
+    print('hello world!')
+
+say_hello()
+
+
