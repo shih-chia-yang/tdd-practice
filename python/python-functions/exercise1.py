c08151a9ca52d0759db1dea501c02b23d8b2cf97
jiayang <jiayang@yuntech.edu.tw>
2021-02-22 16:15:55 +0800

    <refactor>:(add second optional param)</refactor>
    update the code to include a second optional input parameter
    set default value

--- python/python-functions/exercise1.py
+++ python/python-functions/exercise1.py
@@ -3,10 +3,15 @@
 # a pair parentheses symbols (), input parameters are defined within these parantheses
 # a colon symbol : to end the function's signature
 
-def say_hello(name='world'):
-    print(f'hello {name}!')
+def say_hello(name='world',greeting=None):
+    if greeting==None:
+        print(f'hello {name}!')
+    else:
+        print(f'{greeting} {name}!')
 
 say_hello()
 say_hello('Bob')
+say_hello(greeting='howdy')
+say_hello('Bob','howdy')
 
 
