a95df465397d33ce3895e8f52c611e15264767fe
jiayang <jiayang@yuntech.edu.tw>
2021-02-22 16:13:25 +0800

    <refactor>: (param set default value) </refactor>

--- python/python-functions/exercise1.py
+++ python/python-functions/exercise1.py
@@ -3,9 +3,10 @@
 # a pair parentheses symbols (), input parameters are defined within these parantheses
 # a colon symbol : to end the function's signature
 
-def say_hello(name):
+def say_hello(name='world'):
     print(f'hello {name}!')
 
+say_hello()
 say_hello('Bob')
 
 
