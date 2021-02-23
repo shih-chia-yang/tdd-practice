dddd72cb6c9a689ca16086f9fe95145f3de21f46
jiayang <jiayang@yuntech.edu.tw>
2021-02-23 22:13:13 +0800

    <refactor>:(use multi methods)</refactor>
    use multi methods and create dict
    to replace multi if elif else

--- python/python-if/challenge.py
+++ python/python-if/challenge.py
@@ -1,9 +1,22 @@
 # print()
+
+def exiting():
+    return ("Exiting")
+
+def complete():
+    return ('Continuing...','Complete!')
+
 ans=input('Would you like to continue?')
-if ans in ('no','n'):
-    print('Exiting')
-elif ans in ('yes','y'):
-    print('Continuing...')
-    print('COmplete!')
-else:
-    print('Please try again and respond with yes or no')
+func_dict={"no":exiting(),"n":exiting(),"yes":complete(),"y":complete()}
+messages=func_dict[ans]
+for message in messages:
+    print(message)
+
+# python 沒有switch-case
+# 可使用 
+# def do_a_stuff():
+# def do_b_stuff():
+# def do_c_stuff():
+# func_dict={"a":do_a_stuff(),"b":do_b_stuff(),"c":do_c_stuff()}
+# x='a'
+# func_dict[x]()
