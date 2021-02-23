7d9d3686d4c9633a0bddce44b171bab3108824d5
jiayang <jiayang@yuntech.edu.tw>
2021-02-23 22:37:40 +0800

    <refactor>: (use dict comprehension) </refactor>

--- python/python-if/challenge.py
+++ python/python-if/challenge.py
@@ -1,14 +1,21 @@
 # print()
 
 def exiting():
-    return ("Exiting")
+    return ['Exiting']
 
 def complete():
-    return ('Continuing...','Complete!')
+    return ['Continuing...','Complete!']
+
+def Merge(dict1, dict2):
+    return(dict2.update(dict1))
 
 ans=input('Would you like to continue?')
-func_dict={"no":exiting(),"n":exiting(),"yes":complete(),"y":complete()}
-messages=func_dict[ans]
+dict1={item:complete() for item in ["yes","y"]}
+dict2={item:exiting() for item in ["no","n"]}
+
+Merge(dict1, dict2)
+# func_dict={"no":exiting(),"n":exiting(),"yes":complete(),"y":complete()}
+messages=dict2[ans]
 for message in messages:
     print(message)
 
