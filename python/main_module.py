de6b73d3c2a1d9dd18499165b3c7bdbe4e74cfa6
jiayang <jiayang@yuntech.edu.tw>
2021-02-28 09:14:21 +0800

    <feature>: (import main_module) </feature>
    main_module.py
    refactor create_divider
    add parameter align to change words align style
    
    python-if import main_module
    add comment

--- python/main_module.py
+++ python/main_module.py
@@ -1,7 +1,7 @@
-def create_divider(title,count=0,symbol=" "):
+def create_divider(title,count=0,align="^",symbol=" "):
     length=80
     while count!=0:count -=1;print(' ')
-    else:print(f'{title:{symbol}^{length}}')
+    else:print(f'{title:{symbol}{align}{length}}')
     print(' ')
 
 
