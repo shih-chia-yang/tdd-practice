260e62b4f9fbc18bff1fb601f994b0f289ca2a74
jiayang <jiayang@yuntech.edu.tw>
2021-03-02 07:17:48 +0800

    <refactor>: (GetContent add command line args) </refactor>
    add ArgumentParser
    當執行時有命令列參數 - l 時，只顯示行數
    當執行時有命令列參數 - w 時，顯示單字數
    當執行時有命令列參數 - c 時，顯示字元數
    沒有任何選項時，顯示上述3個統計資訊，但
    只要有任意一個選項，則僅顯示指定的統計資訊

--- python/python-lists/src/GetContent.py
+++ python/python-lists/src/GetContent.py
@@ -1,15 +1,32 @@
+from argparse import ArgumentParser
+
+"""get content and count word , line ,char"""
 class GetContent:
-    def __init__(self) :
+    def __init__(self,path) :
         self.word_count=0
         self.char_count=0
         self.line_count=0
+        self.file_path=path
+        self.result_list={}
         pass
 
-    def get_content(self,path,func):
-        with open(path) as content:
+    def count(self):
+        self.get_content(self.line_func)
+        self.get_content(self.word_func)
+        self.get_content(self.char_func)
+
+    def get_content(self,func):
+        with open(self.file_path) as content:
             lines=content.read().split("\n")
             for line in lines:
                 func(line)
+        line_result=f'this file {self.file_path} has lines count :{self.line_count}'
+        word_result=f'this file {self.file_path} has lines count :{self.word_count}'
+        char_result=f'this file {self.file_path} has lines count :{self.char_count}'
+        # self.result_list.update({"intput_file":[line_result,word_result,char_result]})
+        self.result_list.update({"line_func":line_result})
+        self.result_list.update({"word_func":word_result})
+        self.result_list.update({"char_func":char_result})
     
     def line_func(self,line):
         self.line_count+=1
@@ -19,3 +36,26 @@ class GetContent:
     
     def char_func(self,line):
         self.char_count+=len(line)
+
+def main():
+    
+    parser=ArgumentParser()
+    parser.add_argument("intput_file",type=str,help="read data from this file")
+    parser.add_argument("-l","--line",action="store_true",dest="line_func",default=False,help="show this file how many lines count")
+    parser.add_argument("-w","--word",action="store_true",dest="word_func",default=False,help="show this file how many words count")
+    parser.add_argument("-c","--char",action="store_true",dest="char_func",default=False,help="show this file how many chars count")
+    args=parser.parse_args()
+    getcontent=GetContent(args.__dict__["intput_file"])
+    getcontent.count()
+    show=False
+    for func in args.__dict__:
+        if func in getcontent.result_list and bool(args.__dict__[func]):
+            print(getcontent.result_list[func])
+            show=True
+    if not show :
+        for result in getcontent.result_list.values():
+            print(result)
+    
+
+if __name__=='__main__':
+    main()
