726fc6f07cd73dd9a223eb105838cb47d9b92ed0
jiayang <jiayang@yuntech.edu.tw>
2021-03-01 08:16:43 +0800

    <feature>: (add yield & yield from practice) </feature>

--- python/python-functions/generator.py
+++ python/python-functions/generator.py
@@ -17,3 +17,47 @@ for i in count_to_four():
     print(i)
 
 print(2 in count_to_four())
+
+# yield from 
+#可將多個generator串聯在一起，yield from 的行為與yield from相同，它將產生器機制委託給另一個產生器
+
+def subgen(x):
+    for i in range(x):
+        yield i
+
+def gen(y):
+    yield from subgen(y)
+
+for q in gen(6):
+    print(q)
+
+# Tip
+
+def get_teacher_id():
+    pass
+def get_student_id():
+    pass
+
+def gen_alluser():
+    for id in get_teacher_id():
+        # do something
+        yield id
+    for id in get_student_id():
+        # do something
+        yield id
+
+#using yield from refactor
+
+def gen_teacher():
+    for in get_teacher_id():
+        #do something
+        yield id
+
+def gen_student():
+    for in get_student_id():
+        #do something
+        yield id
+
+def get_alluser():
+    yield from gen_teacher()
+    yield from gen_student()
