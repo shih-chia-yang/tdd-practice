5a6a7cf4de595973a877b55579f573d772f327c0
jiayang <jiayang@yuntech.edu.tw>
2021-03-03 22:04:45 +0800

    <refactor>: (ch14_challenge add testdir) </refactor>
    add testdir and put some test file let unittest test function
    refactor ch14_challengeTests
    add main() function

--- python/python-filesystem/ch12_4/ch14_challenge.py
+++ python/python-filesystem/ch12_4/ch14_challenge.py
@@ -1,9 +1,17 @@
 from pathlib import  Path
 
+total_size=0
+
 def measure_size():
-    path=Path(".")
-    total_size=0
+    path=Path("./testdir")
+    global total_size
     for file in path.glob("*.py"):
         if file.is_symlink() is False:
             total_size+=file.stat().st_size 
     return total_size
+
+def main():
+    measure_size()
+
+if __name__=="__main__":
+    main()
--- python/python-filesystem/ch12_4/ch14_challengeTests.py
+++ python/python-filesystem/ch12_4/ch14_challengeTests.py
@@ -1,4 +1,5 @@
 from unittest import TestCase
+import unittest
 import ch14_challenge
 
 class ChallengeTest(TestCase):
@@ -7,4 +8,9 @@ class ChallengeTest(TestCase):
     
     def test_all_py_file_size_in_directory(self):
         size=ch14_challenge.measure_size()
-        self.assertEqual(3942,size)
+        self.assertEqual(12322,size)
+
+def main():
+    unittest.main()
+if __name__=="__main__":
+    main()
