b226bc8050815d8c022ca8f5430fdb2147421c96
jiayang <jiayang@yuntech.edu.tw>
2021-03-11 16:29:28 +0800

    <feature>: (add practice calculate airport weather data) </feature>
    clean data
    spilt to each row to list or dict
    calculate every year avg rains
    calculate every year max temperature
    calculate every year min temperature

--- python/python-practice/src/noaa/https_download.py
+++ python/python-practice/src/noaa/https_download.py
@@ -1,35 +1,56 @@
 #使用http/https下載
 #python requests
 #linux curl wget
-import requests
+import requests,sys
 
-data_row=[]
 yearlist={}
-every_max_tempure={}
+each_year_avg_rain={}
+each_year_max_tmp={}
+each_year_min_tmp={}
 
 def get_data(url):
     response=requests.get(url)
     return response.text
 
 def clear_data(content:str):
-    global yearlist
     lines=content.split("\r\n")[7:]
     for line in lines:
-        fields= [x for x in line.split(" ") if x][0:7]
-        year,month,max,min,af,rain,sun=tuple(fields)
-        if year not in yearlist:
-            data_row.append(fields)
-            # yearlist.setdefault(year,data_row)
-            yearlist[year]=data_row
-            # yearlist[year]=data_row
-            every_max_tempure.setdefault(year)
-            every_max_tempure[year]=max
-        else:
-            yearlist[year].append(fields)
-            if float(max) > float(every_max_tempure[year]):
-                every_max_tempure[year]=max
+        extract(line)
 
-data=get_data("http://www.metoffice.gov.uk/pub/data/weather/uk/climate/stationdata/heathrowdata.txt")
-clear_data(data)
-# print(every_max_tempure['2002'])
-    
+def calculate():
+    for year in yearlist.keys():
+        months=yearlist[year]
+        max_temp=max([float(month[1])  for  month in months])
+        min_temp=min([float(month[2])  for  month in months])
+        rains=[float(month[4])  for  month in months]
+        avg_rains=round(sum(rains)/len(rains),2)
+        each_year_avg_rain[year]=avg_rains
+        each_year_max_tmp[year]=max_temp
+        each_year_min_tmp[year]=min_temp
+
+def get_detail(line):
+    fields=[x for x in line.split(" ") if x][0:7]
+    year,month,max,min,af,rain,sun= tuple(fields)
+    return year,month,max,min,af,rain,sun
+
+def extract(line):
+    global yearlist
+    year,month,max,min,af,rain,sun= get_detail(line)
+    if year not in yearlist:
+        yearlist[year]=[[month,max,min,af,rain,sun]]
+    else:
+        yearlist[year].append([month,max,min,af,rain,sun])
+
+def main():
+    data=get_data("http://www.metoffice.gov.uk/pub/data/weather/uk/climate/stationdata/heathrowdata.txt")
+    clear_data(data)
+    calculate()
+    print(each_year_avg_rain)
+    print(each_year_max_tmp)
+    print(each_year_min_tmp)
+
+if __name__ == '__main__':
+    main()
+
+
+
