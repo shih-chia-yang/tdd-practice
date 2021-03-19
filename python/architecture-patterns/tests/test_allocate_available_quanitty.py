9c46876644a0655ae16f6f3da5fa0ddb7d59767f
jiayang <jiayang@yuntech.edu.tw>
2021-03-19 21:49:51 +0800

    <feature>: (impl can_allocate(line)) </feature>
    if batch.sku = line.sku then
    compare batch.available_quantity and line.qty
    if out of batch : batch.qty<line.qty return false
    if available allocate: batch.qty>line.qty return True

--- python/architecture-patterns/tests/test_allocate_available_quanitty.py
+++ python/architecture-patterns/tests/test_allocate_available_quanitty.py
@@ -5,6 +5,10 @@ sys.path.append("../src")
 from Batch import Batch
 from OrderLine import OrderLine
 
+def fake_can_allocate_batch_list():
+        return [("desk", 20, 2,True),
+            ("chair", 2, 10,False)]
+
 def fake_batch_and_line(sku,batch_qty,line_qty):
     return (Batch("batch-001",sku,batch_qty,eta=datetime.date.today),
             OrderLine("order-001",sku,line_qty))
@@ -17,8 +21,9 @@ def test_allocating_to_a_batch_reduces_the_available_quantity():
     assert batch.available_quantity==(fake_batch_qty-fake_line_qty)
 
 def test_can_allocate_if_available_greater_than_required():
-    out_of_batch ,line = fake_batch_and_line("small-table",2,20)
-    assert out_of_batch.can_allocate(line) is False
+    for sku,batch_qty,line_qty,expected in fake_can_allocate_batch_list():
+        batch ,line = fake_batch_and_line(sku,batch_qty,line_qty)
+        assert batch.can_allocate(line) is expected
 # def main():
 #     test_allocating_to_a_batch_reduces_the_available_quantity()
 
