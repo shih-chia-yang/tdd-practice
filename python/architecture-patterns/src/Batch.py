159021ea8ca73f5c2583aa3a06c94f202fb18a44
jiayang <jiayang@yuntech.edu.tw>
2021-03-19 22:04:52 +0800

    <test>: (add test_can_not_allocate_if_skus_do_not_match) </test>
    add test method
    different skus should be return false

--- python/architecture-patterns/src/Batch.py
+++ python/architecture-patterns/src/Batch.py
@@ -13,5 +13,5 @@ class Batch():
         self.available_quantity-=line.qty
         
     def can_allocate(self,line:OrderLine)->bool:
-        return self.sku==line.sku and self.available_quantity>line.qty
+        return self.sku==line.sku and self.available_quantity>=line.qty
         
--- python/architecture-patterns/tests/test_allocate_available_quanitty.py
+++ python/architecture-patterns/tests/test_allocate_available_quanitty.py
@@ -6,12 +6,16 @@ from Batch import Batch
 from OrderLine import OrderLine
 
 def fake_can_allocate_batch_list():
-        return [("desk", 20, 2,True),
-            ("chair", 2, 10,False)]
+        return [(fake_batch_and_line("desk", 20, 2),True),
+            (fake_batch_and_line("chair", 2, 10),False),
+            (fake_batch_and_line("elegant-lamp",5,5),True)]
 
-def fake_batch_and_line(sku,batch_qty,line_qty):
-    return (Batch("batch-001",sku,batch_qty,eta=datetime.date.today),
-            OrderLine("order-001",sku,line_qty))
+def fake_batch_and_line(sku,batch_qty,line_qty,line_sku=""):
+    if  bool(line_sku) is False:
+        line_sku=sku
+    batch=Batch("batch-001",sku,batch_qty,eta=datetime.date.today)
+    line=OrderLine("order-001",line_sku,line_qty)
+    return (batch,line)
 
 def test_allocating_to_a_batch_reduces_the_available_quantity():
     fake_batch_qty=20
@@ -21,9 +25,14 @@ def test_allocating_to_a_batch_reduces_the_available_quantity():
     assert batch.available_quantity==(fake_batch_qty-fake_line_qty)
 
 def test_can_allocate_if_available_greater_than_required():
-    for sku,batch_qty,line_qty,expected in fake_can_allocate_batch_list():
-        batch ,line = fake_batch_and_line(sku,batch_qty,line_qty)
+    for actual,expected in fake_can_allocate_batch_list():
+        batch,line=actual
         assert batch.can_allocate(line) is expected
+
+def test_can_not_allocate_if_skus_do_not_match():
+    batch,line =fake_batch_and_line("table",10,5,"chair")
+    assert batch.can_allocate(line) is False
+
 # def main():
 #     test_allocating_to_a_batch_reduces_the_available_quantity()
 
